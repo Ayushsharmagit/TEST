abab67b1114a23ef863cc14f249c06e3e38f8cc5
Ayush Sharma <42935052+Ayushsharmagit@users.noreply.github.com>
2019-01-17 20:32:03 +0530

    Update UserDAO.java

--- src/org/project/testengine/user/dao/UserDAO.java
+++ src/org/project/testengine/user/dao/UserDAO.java
@@ -63,46 +63,5 @@ public class UserDAO {
 			
 		}
 	}
-	
-	
-	
-	
-/*	public static void main(String[] args)  {
-		Connection con=null;
-		ResultSet rs=null;
-		//step-1 load a driver/class
-		PreparedStatement pstmt=null;
-			System.out.println("Driver Load");
-			
-			
-			//step-2 Create A connection
-			 con =null;
-			
-			if(con!=null){
-				System.out.println("Connection Created");
-			}
-			//Db Query
-			pstmt= con.prepareStatement("select * from users");
-			 rs=pstmt.executeQuery();//table record
-			while(rs.next()) { 
-				System.out.println("\n userid:-"+rs.getString("userid")+
-						"\n Password:-"+rs.getString("password"));	
-			}
-			
-				
-				
-		finally {
-				if(rs!=null) {
-		
-			rs.close();}
-				if (pstmt!=null) {
-			pstmt.close();
-				}
-				if (con!=null) {
-			con.close();
-				
-			}
-			
-			}
-		}*/
+
 }
